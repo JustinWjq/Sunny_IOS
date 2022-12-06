77eb81daa3e8c74cb4e1a976d19abd409774d56a
QSZX001 <542829817@qq.com>
2022-12-06 21:01:00 +0800

    提供例子

--- demo/sdk/TXTWhiteBoard/Classes/Chat/Model/TXTFileModel.h
+++ demo/sdk/TXTWhiteBoard/Classes/Chat/Model/TXTFileModel.h
@@ -11,15 +11,36 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TXTFileModel : NSObject
-
+// 图片类型
+/**
+ TXTFileModel *fileModel = [[TXTFileModel alloc] init];
+ fileModel.pics = @[@"https://wisdom-exhibition-1301905869.cos.ap-shenzhen-fsi.myqcloud.com/testdocument/0jsoaidalsh31nr2bk4c_tiw/picture/1.jpg", @"https://wisdom-exhibition-1301905869.cos.ap-shenzhen-fsi.myqcloud.com/testdocument/0jsoaidalsh31nr2bk4c_tiw/picture/2.jpg"];
+ fileModel.contents = @[@"你是哈回电话阿萨德发生的",@"",@"你是哈回电话阿萨德发生的"];
+ // 当没有图片关键词,数组需要传入对应的@“”空字符串
+ // [[TXTManage sharedInstance] addFileToSdk:FileTypePics fileModel:fileModel];
+ */
 /** pics 图片数组 */
 @property (nonatomic, strong) NSArray *pics;
 /** contents 图片对应的提词器 */
 @property (nonatomic, strong) NSArray *contents;
-
+        
+// 视频类型
+/**
+ TXTFileModel *fileModel = [[TXTFileModel alloc] init];
+ fileModel.videoUrl = @"https://res.qcloudtiw.com/demo/tiw-vod.mp4";
+ [[TXTManage sharedInstance] addFileToSdk:FileTypeVideo fileModel:fileModel];
+ */
 /** videoUrl */
 @property (nonatomic, copy) NSString *videoUrl;
 
+// h5类型
+/**
+ TXTFileModel *fileModel = [[TXTFileModel alloc] init];
+ fileModel.h5Url = @"https://recall-sync-demo.cloud-ins.cn/mirror.html?syncid=51-cvsstest123-1&synctoken=0060490432279104e008daf9a660dfb8d2aIABaoflIqpo4-W91SrtSeG8e-QAQ5_O7_RsAQrms1PxSLJ597XwAAAAAEADKL1Dbsjd_YwEA6AOyN39j";
+ fileModel.name = @"test";
+ [[TXTManage sharedInstance] addFileToSdk:FileTypeH5 fileModel:fileModel];
+ */
+        
 /** h5Url */
 @property (nonatomic, copy) NSString *h5Url;
 /** name */
