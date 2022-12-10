70c3620c24f2e587e06ff70377af8ca846ee3c50
QSZX001 <542829817@qq.com>
2022-12-10 14:44:00 +0800

    xxxx

---
+++ demo/sdk/TXTWhiteBoard/Classes/Chat/Controller/TXTTestViewController.h
@@ -0,0 +1,18 @@
+//
+//  TXTTestViewController.h
+//  TXTWhiteBoard
+//
+//  Created by jiangyongjian on 2022/12/10.
+//  Copyright © 2022 洪青文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TXTTestViewController : UIViewController
+/// click
+- (void)click;
+@end
+
+NS_ASSUME_NONNULL_END
