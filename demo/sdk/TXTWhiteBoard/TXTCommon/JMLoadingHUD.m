7fc5e2e68c8a9087b843b23ac542785c8dd4394d
aiquantong <aiquantong@163.com>
2023-01-03 22:49:39 +0800

    feat:TXTMBProgressHUD视角残留

--- demo/sdk/TXTWhiteBoard/TXTCommon/JMLoadingHUD.m
+++ demo/sdk/TXTWhiteBoard/TXTCommon/JMLoadingHUD.m
@@ -34,7 +34,7 @@
 {
     self = [super init];
     if (self) {
-        [[[UIApplication sharedApplication] keyWindow] addSubview:self.loadingHUD];
+        
     }
     return self;
 }
@@ -45,6 +45,7 @@
         if (hud->_isShow) {
             return;
         }
+        [[[UIApplication sharedApplication] keyWindow] addSubview:hud.loadingHUD];
         [hud.loadingHUD showAnimated:YES];
         hud->_isShow = YES;
     });
@@ -56,6 +57,8 @@
         hud.loadingHUD.label.text = @"";
         [hud.loadingHUD hideAnimated:YES];
         hud->_isShow = NO;
+        [hud.loadingHUD removeFromSuperview];
+        hud.loadingHUD = nil;
     });
 }
 
